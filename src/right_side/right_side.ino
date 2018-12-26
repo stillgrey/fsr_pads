0fc960e4725728e2d4108469b75a0467577a1daf
feedbacker <feedbacker@users.noreply.github.com>
2018-12-26 15:24:14 -0600

    oh my god. so sorry. i forgot to uncomment the keyboard functions.

--- src/right_side/right_side.ino
+++ src/right_side/right_side.ino
@@ -2,22 +2,22 @@
 
 #define BASE_PRESSURE  0
 
-#define LEFT_PRESSURE  375       
+#define LEFT_PRESSURE  375
 #define DOWN_PRESSURE  437
 #define RIGHT_PRESSURE 380
 #define UP_PRESSURE    345
 
 #include <Keyboard.h>
- 
+
 int LURD_pins[4] = {0, 1, 2, 3};
 int LURD_State[4] = {0, 0, 0, 0};
 int LURD_pressures[4] = {LEFT_PRESSURE, UP_PRESSURE, RIGHT_PRESSURE, DOWN_PRESSURE};
 char LURD_Keys[5] = "awds";
 const unsigned int MAX_INPUT = 50;
 
-void setup(void) 
+void setup(void)
 {
-  Serial.begin(9600);  
+  Serial.begin(9600);
   Keyboard.begin();
 }
 
@@ -25,11 +25,11 @@ void setup(void)
 String format_pad_sensitivity (int input)
 {
   String retval = "";
-  
-  if (input < 100) retval += "0"; 
+
+  if (input < 100) retval += "0";
   if (input < 10) retval += "0";
   retval += String(input);
-  
+
   return retval;
 }
 
@@ -37,7 +37,7 @@ String format_pad_sensitivity (int input)
 void process_data (char * data)
 {
   //TODO: Remove this line. This line is unnecessary since the data comes with null terminator
-  //      before this function gets called. 
+  //      before this function gets called.
   data[4] = 0;
 
   // Check to see if the first byte is either 0,1,2,3.
@@ -50,14 +50,14 @@ void process_data (char * data)
   {
     Serial.print(headers[i]);
     Serial.print(format_pad_sensitivity(LURD_pressures[i]));
-    Serial.println(",");  
-  }  
+    Serial.println(",");
+  }
   Serial.println("");
 }
 
 // Puts the serial commands in an array, terminates the commands with a null terminator
 // and sends it off to be processed.
-  
+
 void processIncomingByte (const byte inByte)
 {
   static char input_line [MAX_INPUT];
@@ -68,8 +68,8 @@ void processIncomingByte (const byte inByte)
     case '\n': // Newline signifies the end of the text
       input_line [input_pos] = 0; // Add null terminator at the end.
       process_data (input_line);
-      
-      input_pos = 0; // Reset the input_pos so that the buffer is in the beginning and ready to be written. 
+
+      input_pos = 0; // Reset the input_pos so that the buffer is in the beginning and ready to be written.
       break;
 
     case '\r': // Discard carriage return. Linux and Windows have different line endings but they both end in '\n.'
@@ -79,14 +79,14 @@ void processIncomingByte (const byte inByte)
     default: // Add each byte to each index of the array until the buffer runs out.
       if (input_pos < (MAX_INPUT - 1)) input_line[input_pos++] = inByte;
       break;
-  }    
-} 
+  }
+}
 
 int counter = 0;
 void loop(void)
-{ 
+{
  counter = (counter + 1) % 10; // Adjust the command polling rate here.
- 
+
  // If there are commands that needs to processed, process them.
  if (counter == 0)
  {
@@ -100,16 +100,16 @@ void loop(void)
      {
       if (LURD_State[i] == 0)
       {
-        //Keyboard.press(LURD_Keys[i]);
+        Keyboard.press(LURD_Keys[i]);
         LURD_State[i] = 1;
       }
      }
-     
+
      else
      {
        if (LURD_State[i] == 1)
        {
-         //Keyboard.release(LURD_Keys[i]);
+         Keyboard.release(LURD_Keys[i]);
          LURD_State[i] = 0;
        }
      }
@@ -119,7 +119,7 @@ void loop(void)
   fsrReading = analogRead(0);
   Serial.print("Analog reading (L)= ");
   Serial.println(fsrReading);
- 
+
   fsrReading = analogRead(1);
   Serial.print("Analog reading (U)= ");
   Serial.println(fsrReading);
